cefd27f943a1e6ae6398f1cec94455008e020a13
Tawhid Zihad <tawhidzihadb1@gmail.com>
2026-06-26 18:58:14 +0600

    Create expense api added

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import { Expense } from "./types";
 dotenv.config();
 
 const app: Application = express();
@@ -12,8 +13,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.DATABASE_URI;
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.DATABASE_URI!;
 
 const client = new MongoClient(uri, {
 	serverApi: {
@@ -27,6 +28,25 @@ async function run() {
 	try {
 		await client.connect();
 
+		// database collections
+		const db = client.db("expense-tracker");
+		const expensesCollection = db.collection<Expense>("expenses");
+
+		// Create new Expensess & Send Success Message
+		app.post("/api/expense", async (req: Request, res: Response) => {
+			const expenseData: Expense = req.body;
+
+			console.log(expenseData);
+
+			const result = await expensesCollection.insertOne(expenseData);
+
+			res.status(201).json({
+				success: true,
+				message: "Expense created successfully.",
+				data: result,
+			});
+		});
+
 		await client.db("admin").command({ ping: 1 });
 		console.log(
 			"Pinged your deployment. You successfully connected to MongoDB!",
@@ -38,7 +58,10 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-	res.json("Expense Trackin Server is Running!");
+	res.json({
+		success: true,
+		message: "Expense Tracking Server is Running!",
+	});
 });
 
 app.listen(port, () => {
