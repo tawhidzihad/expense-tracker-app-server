aaa64328da08a96bfba543d94efd7a360b6d0edd
Tawhid Zihad <tawhidzihadb1@gmail.com>
2026-06-27 00:46:03 +0600

    Get all expenses api added with pagination data, filter by category, total expense amount, and pie chart summary

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 const uri = process.env.DATABASE_URI!;
 
 const client = new MongoClient(uri, {
@@ -35,14 +35,122 @@ async function run() {
 		// Create new Expensess & Send Success Message
 		app.post("/api/expense", async (req: Request, res: Response) => {
 			const expenseData: Expense = req.body;
-
-			console.log(expenseData);
-
 			const result = await expensesCollection.insertOne(expenseData);
 
 			res.status(201).json({
 				success: true,
 				message: "Expense created successfully.",
+				data: result,
+			});
+		});
+
+		// Get all expenses with pagination, category filter, total expense amount, and pie chart summary
+		app.get("/api/expense", async (req: Request, res: Response) => {
+			const page = Number(req.query.page) || 1;
+			const limit = Number(req.query.limit) || 6;
+			const category = req.query.category as string | undefined;
+
+			const skip = (page - 1) * limit;
+
+			const filter: Partial<Expense> = {};
+
+			if (category) {
+				filter.category = category;
+			}
+
+			// Get expenses
+			const expenses = await expensesCollection
+				.find(filter)
+				.sort({ _id: -1 })
+				.skip(skip)
+				.limit(limit)
+				.toArray();
+
+			// Count total expenses
+			const totalExpenses =
+				await expensesCollection.countDocuments(filter);
+
+			// Calculate total expense amount
+			const totalExpenseResult = await expensesCollection
+				.aggregate([
+					{
+						$match: filter,
+					},
+					{
+						$group: {
+							_id: null,
+							totalAmount: {
+								$sum: "$amount",
+							},
+						},
+					},
+				])
+				.toArray();
+
+			const totalExpenseAmount = totalExpenseResult[0]?.totalAmount || 0;
+
+			// Pie chart summary
+			const categorySummary = await expensesCollection
+				.aggregate([
+					{
+						$match: filter,
+					},
+					{
+						$group: {
+							_id: "$category",
+							totalAmount: {
+								$sum: "$amount",
+							},
+						},
+					},
+					{
+						$project: {
+							_id: 0,
+							category: "$_id",
+							totalAmount: 1,
+						},
+					},
+				])
+				.toArray();
+
+			res.status(200).json({
+				success: true,
+				message: "Expenses fetched successfully.",
+				data: {
+					expenses,
+
+					pagination: {
+						currentPage: page,
+						totalPages: Math.ceil(totalExpenses / limit),
+						totalExpenses,
+						limit,
+					},
+
+					summary: {
+						totalExpenseAmount,
+						categorySummary,
+					},
+				},
+			});
+		});
+
+		// Update single expense data
+		app.patch("/api/expense/:id", async (req: Request, res: Response) => {
+			const id = req.params.id as string;
+
+			const filter = {
+				_id: new ObjectId(id),
+			};
+
+			const updatedData: Partial<Expense> = req.body;
+
+			const result = await expensesCollection.updateOne(filter, {
+				$set: updatedData,
+			});
+
+			res.status(200).json({
+				success: true,
+				message: "Expense edited successfully.",
 				data: result,
 			});
 		});
